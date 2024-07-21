f05045a83318b277c61d0d9ba7430e7a23ac319d
jossi30 <yosiefghaylay@gmail.com>
2024-07-21 22:59:13 +0300

    added contact section

---
+++ src/Pages/Home/ContactMe.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default function ContactMe() {
+  return (
+    <section id="contact" className="contact--section">
+      <div>
+        <p className="sub--title">Get In Touch</p>
+        <h2>Contact Us</h2>
+        <p className="text-lg">
+        Contact us for any questions or to learn more about our services!
+        </p>
+      </div>
+      <form className="contact--form--container">
+        <div>
+          <label htmlFor="name" className="contact--label">
+            <span className="text-md">Name</span>
+            <input
+              type="text"
+              className="contact--input text-md"
+              name="name"
+              id="name"
+              required
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email" className="contact--label">
+            <span className="text-md">Email</span>
+            <input
+              type="email"
+              className="contact--input text-md"
+              name="email"
+              id="email"
+              required
+            />
+          </label>
+        </div>
+        <label htmlFor="message" className="contact--label">
+          <span className="text-md">Message</span>
+          <textarea
+            className="contact--input text-md"
+            id="message"
+            name="message"
+            rows="8"
+            placeholder="Type your message..."
+            required
+          />
+        </label>
+        <div>
+          <button className="btn btn-primary contact--form--btn" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
