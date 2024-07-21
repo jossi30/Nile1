e40452f510b2e4fc7b75c535a15ff4fa7571e4e9
jossi30 <yosiefghaylay@gmail.com>
2024-07-21 22:57:34 +0300

    added about me section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-nile.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">Who We Are?</p>
+          <h1 className="skills-section--heading">About Us</h1>
+          <p className="hero--section-description">
+          At Nile Marketing Agency, we specialize in helping small service-based businesses thrive in the competitive digital landscape. Our comprehensive suite of digital marketing services is designed to drive growth, enhance online presence, and deliver measurable results.   
+          </p>
+          <p className="hero--section-description">
+          We understand the unique challenges faced by small businesses and are committed to providing tailored solutions that meet your specific needs.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
